33865c79869f837bd9e8eb7da9de4754ba843ae2
sahulchowdarykodali <sahulchowdarykodali@10.0.0.183>
2020-01-05 20:43:55 -0600

    @PathVariable Annotation Demonstartion

--- src/main/java/com/springboot/microservicesdemo/web/DemoController.java
+++ src/main/java/com/springboot/microservicesdemo/web/DemoController.java
@@ -1,6 +1,7 @@
 package com.springboot.microservicesdemo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.microservicesdemo.model.HelloWorldBeanDemonstartion;
@@ -15,5 +16,10 @@ public class DemoController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBeanDemonstartion helloWorldBeanDemo() {
 		return new HelloWorldBeanDemonstartion("Controller Returning Bean Demo");
+	}
+	
+	@GetMapping(path = "/hello-world-pathVariable/{param}")
+	public HelloWorldBeanDemonstartion helloWorldPathVariableDemo(@PathVariable String param) {
+		return new HelloWorldBeanDemonstartion(String.format("Hello, this is %s Controller Demonstrating the usage of Path Variable!!!", param));
 	}
 }
