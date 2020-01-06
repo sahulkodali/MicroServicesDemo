e47be3e82e8fa040b977cb0989163bdc34ee0899
sahulchowdarykodali <sahulchowdarykodali@10.0.0.183>
2020-01-05 19:57:39 -0600

    @GetMappingDemo

--- src/main/java/com/springboot/microservicesdemo/web/DemoController.java
+++ src/main/java/com/springboot/microservicesdemo/web/DemoController.java
@@ -1,13 +1,12 @@
 package com.springboot.microservicesdemo.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-	@RequestMapping(method= RequestMethod.GET, path = "/hello-world")public String helloWorld() {
+	@GetMapping(path = "/hello-world")public String helloWorld() {
 		return "Hello World";
 	}
 }
