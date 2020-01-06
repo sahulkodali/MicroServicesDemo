41452f718305bd88c0437b42f248b86b20df2340
sahulchowdarykodali <sahulchowdarykodali@10.0.0.183>
2020-01-05 18:54:35 -0600

    Initial Commit

---
+++ src/main/java/com/springboot/microservicesdemo/web/DemoController.java
@@ -0,0 +1,13 @@
+package com.springboot.microservicesdemo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping(method= RequestMethod.GET, path = "/hello-world")public String helloWorld() {
+		return "Hello World";
+	}
+}
