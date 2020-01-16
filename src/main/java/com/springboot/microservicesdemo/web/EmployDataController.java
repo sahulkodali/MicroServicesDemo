fc454b31957178a8bfd9a3d5f5a55a9d402f88d3
sahulchowdarykodali <sahulchowdarykodali@10.0.0.183>
2020-01-16 11:00:42 -0600

    ServletURIComponentBuilder for sending the data when employ is created

--- src/main/java/com/springboot/microservicesdemo/web/EmployDataController.java
+++ src/main/java/com/springboot/microservicesdemo/web/EmployDataController.java
@@ -1,13 +1,16 @@
 package com.springboot.microservicesdemo.web;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.microservicesdemo.model.Employ;
 import com.springboot.microservicesdemo.repository.EmployDataAccessService;
@@ -29,9 +32,10 @@ public class EmployDataController {
 	}
 	
 	@PostMapping("/addemploytotherepository")
-	public Employ addemploytotherepository(@RequestBody Employ employ) {
+	public ResponseEntity addemploytotherepository(@RequestBody Employ employ) {
 		Employ savedEmploy = employDataService.addEmploy(employ);
-		return savedEmploy;
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedEmploy.getEmployeeId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 	
 }
